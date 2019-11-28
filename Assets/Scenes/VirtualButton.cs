bdf3589197e40cd8eb371dbdcb66f96905e3a0aa
Busa karthikeyu <busa@Busas-MacBook-Air.local>
2019-11-28 19:43:40 +0100

    AR application for virtual button

---
+++ Assets/Scenes/VirtualButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject Sun;
+    public GameObject Moon;
+    public GameObject Boat;
+    VirtualButtonBehaviour virtualButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualButton = GetComponentInChildren<VirtualButtonBehaviour> ();
+        virtualButton.RegisterEventHandler(this);
+        Sun.SetActive(true);
+        Moon.SetActive(false);
+        Boat.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Moon.SetActive(true);
+        Sun.SetActive(false);
+        Boat.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Sun.SetActive(true);
+        Moon.SetActive(false);
+        Boat.SetActive(false);
+    }
+}
+
+
